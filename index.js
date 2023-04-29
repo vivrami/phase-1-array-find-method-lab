598d72092301277eefdea5920f50fde497c5869b
vivianna ramirez <vivrami2001@gmail.com>
2023-04-29 11:38:20 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function superbowlWin(arrayObjs){
+    let output = arrayObjs.find(
+    function(element){
+        if(element.result === `W`){
+            return element
+        }
+    })
+if(output){
+    return output.year
+}
+else{
+    return undefined
+}
+}
